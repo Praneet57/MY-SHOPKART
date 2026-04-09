93e89d8a0ba18eb7af56c568b1e4d4e4695ea45f
PRANEET S <praneetsatheeshraj@gmail.com>
2026-04-09 16:49:07 +0530

    Use pure SQLite - remove dj_database_url

--- praneet_project/settings.py
+++ praneet_project/settings.py
@@ -77,15 +77,11 @@ WSGI_APPLICATION = 'praneet_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600,
-        conn_health_checks=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
